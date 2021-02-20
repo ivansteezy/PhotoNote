19d863a0cb159d44e0d94c8e900fb9e27d28dcfe
Ivan Ayala <network.9961@gmail.com>
2021-02-20 00:43:59 -0600

    adding alerts, redirect from login to register

--- PhotoNote/app/src/main/java/com/syro/photonote/MainActivity.java
+++ PhotoNote/app/src/main/java/com/syro/photonote/MainActivity.java
@@ -1,14 +1,57 @@
 package com.syro.photonote;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void OpenRegisterActivity(View view)
+    {
+        Intent myIntent = new Intent(MainActivity.this, RegisterActivity.class);
+        MainActivity.this.startActivity(myIntent);
+    }
+
+    public void InputErrorDialog(View view)
+    {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Error");
+        alert.setMessage("Datos ingresados incorrectos");
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog dialog = alert.create();
+        dialog.show();
+    }
+
+    public void LoginError(View view)
+    {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Error");
+        alert.setMessage("El usuario no existe en la base de datos");
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog dialog = alert.create();
+        dialog.show();
     }
 }
--- PhotoNote/app/src/main/java/com/syro/photonote/RegisterActivity.java
+++ PhotoNote/app/src/main/java/com/syro/photonote/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.syro.photonote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void OpenLoginActivity(View view)
+    {
+        Intent myIntent = new Intent(RegisterActivity.this, MainActivity.class);
+        RegisterActivity.this.startActivity(myIntent);
     }
 }
