ebf1da4703e7732a726d2c8a1f0a1ebb61d53014
Ivan Ayala <network.9961@gmail.com>
2021-02-22 13:18:36 -0600

    detecting which item was pressed

--- PhotoNote/app/src/main/java/com/syro/photonote/MyPhotosRecyclerViewAdapter.java
+++ PhotoNote/app/src/main/java/com/syro/photonote/MyPhotosRecyclerViewAdapter.java
@@ -4,10 +4,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.QuickContactBadge;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.Console;
 
 public class MyPhotosRecyclerViewAdapter extends RecyclerView.Adapter<MyPhotosRecyclerViewAdapter.MyPhotoViewHolder> {
     private int []m_items;
@@ -22,6 +26,14 @@ public class MyPhotosRecyclerViewAdapter extends RecyclerView.Adapter<MyPhotosRe
     public MyPhotoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.my_photo_item, parent, false); // ojo
         MyPhotoViewHolder myPhotoViewHolder = new MyPhotoViewHolder(view);
+
+        myPhotoViewHolder.m_photoContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Se ha presionado un item: " + myPhotoViewHolder.getAdapterPosition());
+            }
+        });
+
         return myPhotoViewHolder;
     }
 
@@ -38,13 +50,15 @@ public class MyPhotosRecyclerViewAdapter extends RecyclerView.Adapter<MyPhotosRe
 
     public class MyPhotoViewHolder extends RecyclerView.ViewHolder
     {
+        LinearLayout m_photoContainer;
         ImageView m_imageView;
         TextView m_textView;
         public MyPhotoViewHolder(@NonNull View itemView)
         {
             super(itemView);
-            m_imageView = itemView.findViewById(R.id.imageItemView);
-            m_textView  = itemView.findViewById(R.id.imageItemText);
+            m_photoContainer = itemView.findViewById(R.id.myPhotoItemContainer);
+            m_imageView      = itemView.findViewById(R.id.imageItemView);
+            m_textView       = itemView.findViewById(R.id.imageItemText);
         }
     }
 }
