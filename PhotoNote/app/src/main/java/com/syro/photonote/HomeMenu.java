16f9a16d3a5da435a427961899e6e615cea0fdcd
Ivan Ayala <network.9961@gmail.com>
2021-02-21 20:03:14 -0600

    add three functionality fragments

--- PhotoNote/app/src/main/java/com/syro/photonote/HomeMenu.java
+++ PhotoNote/app/src/main/java/com/syro/photonote/HomeMenu.java
@@ -2,6 +2,8 @@ package com.syro.photonote;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -19,30 +21,39 @@ public class HomeMenu extends AppCompatActivity {
         setContentView(R.layout.activity_home_menu);
 
         m_bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottomNavigationMenu);
-        m_infoTextView = (TextView)findViewById(R.id.infoTextView);
-
+        m_bottomNavigationView.setSelectedItemId(R.id.myPhotosItem);
+        OpenFragment(new MyPhotosFragment());
         m_bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId())
                 {
                     case R.id.myPhotosItem:
-                        m_infoTextView.setText("Mis fotos");
+                        OpenFragment(new MyPhotosFragment());
                         item.setChecked(true);
                     break;
 
                     case R.id.addPhotoItem:
-                        m_infoTextView.setText("Agregar foto");
+                        OpenFragment(new AddPhotosFragment());
                         item.setChecked(true);
                     break;
 
                     case R.id.myProfileItem:
-                        m_infoTextView.setText("Mi perfil");
+                        OpenFragment(new MyProfileFragment());
                         item.setChecked(true);
                     break;
                 }
                 return false;
             }
         });
+    }
+
+    void OpenFragment(Fragment fragment)
+    {
+        Fragment mFragment = null;
+        mFragment = fragment;
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.contentFrame, mFragment).commit();
     }
 }
